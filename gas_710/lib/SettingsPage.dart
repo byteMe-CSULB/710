abb0e1d97ae0ff50229325676507f23d168a5b6c
brandonnhem <brandonnhem@gmail.com>
2020-01-27 13:31:47 -0800

    Added login functionality in SettingsPage.dart, not fully functional

--- gas_710/lib/SettingsPage.dart
+++ gas_710/lib/SettingsPage.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:gas_710/main.dart';
+import 'package:gas_710/auth.dart';
+import 'package:gas_710/AccountPage.dart';
 
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
 
-class SettingsPage extends StatelessWidget {
+class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold
@@ -12,24 +19,54 @@ class SettingsPage extends StatelessWidget {
         title: new Text("Settings Page"),
         backgroundColor: Colors.purple,
       ),
-      body: Builder(
-        builder: (context) => Center(
-          child: RaisedButton(
-            child: Text("Sign In"),
-            onPressed: () => _showToast(context),
-            color: Colors.purple[300],
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            splashColor: Colors.purple[900],
-          ),
-        ),)
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _signInButton(),
+          ],
+        ),
+      )
     );
   }
-}
 
-void _showToast(BuildContext context) {
-  final scaffold = Scaffold.of(context);
-  scaffold.showSnackBar(
-    SnackBar(content: const Text('Signing in..'),
-    action: SnackBarAction(label: 'LOGOUT', onPressed: scaffold.hideCurrentSnackBar))
-  );
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        signInWithGoogle().whenComplete(() {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return AccountPage();
+              },
+            ),
+          );
+        });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
