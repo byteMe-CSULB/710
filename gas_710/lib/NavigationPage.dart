980c544c529cf0f937ae74c578b237aa447564d1
brandonnhem <brandonnhem@gmail.com>
2020-02-10 05:21:19 -0800

    Hard coded directions from X to Y

--- gas_710/lib/NavigationPage.dart
+++ gas_710/lib/NavigationPage.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gas_710/main.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'dart:async';
+
+const double CAMERA_ZOOM = 13;
+const double CAMERA_TILT = 0;
+const double CAMERA_BEARING = 30;
+const LatLng SOURCE_LOCATION = LatLng(33.783022, -118.112858); // CSULB :)
+const LatLng DEST_LOCATION = LatLng(42.6871386, -71.2143403);
 
 class NavigationPage extends StatefulWidget {
   @override
@@ -11,14 +18,53 @@ class NavigationPage extends StatefulWidget {
 class _NavigationPageState extends State<NavigationPage> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng _center = const LatLng(33.783022, -118.112858); // coordinates to CSULB :)
+  // this set will hold my markers
+  Set<Marker> _markers = {};
+  
+  // this will hold the generated polylines
+  Set<Polyline> _polylines = {};
+
+  // this will hold each polyline coordinate as Lat and Lng pairs
+  List<LatLng> polylineCoordinates = [];
+
+  // this is the key object - the PolylinePoints
+  // which generates every polyline between start and finish
+  PolylinePoints polylinePoints = PolylinePoints();
+
+  String googleAPIKey = "AIzaSyDX4p3BL6ceYnQma0S6vMCYQVRKD5P-JT4";
+
+  // for my custom icons
+  BitmapDescriptor sourceIcon;
+  BitmapDescriptor destinationIcon;
+
+  @override
+  void initState() {
+    super.initState();
+    setSourceAndDestinationIcons();
+  }
+
+  void setSourceAndDestinationIcons() async {
+    sourceIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.5), 'assets/driving_pin.png');
+    destinationIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.5),
+        'assets/location_pin.png');
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
+    setMapPins();
+    setPolylines();
   }
 
   @override
   Widget build(BuildContext context) {
+    CameraPosition initialLocation = CameraPosition(
+          zoom: CAMERA_ZOOM,
+          bearing: CAMERA_BEARING,
+          tilt: CAMERA_TILT,
+          target: SOURCE_LOCATION);
+
     return new Scaffold
     (
       drawer: new DrawerCodeOnly(), // provides nav drawer
@@ -27,12 +73,61 @@ class _NavigationPageState extends State<NavigationPage> {
         backgroundColor: Colors.purple,
       ),
       body: GoogleMap(
+          myLocationEnabled: true,
+          compassEnabled: true,
+          tiltGesturesEnabled: false,
+          markers: _markers,
+          polylines: _polylines,
+          mapType: MapType.normal,
           onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 15.0,
-          ),
+          initialCameraPosition: initialLocation,
         ),
     );
+  }
+
+  void setMapPins() {
+      setState(() {
+        // source pin
+        _markers.add(Marker(
+            markerId: MarkerId('sourcePin'),
+            position: SOURCE_LOCATION,
+            icon: sourceIcon));
+        // destination pin
+        _markers.add(Marker(
+            markerId: MarkerId('destPin'),
+            position: DEST_LOCATION,
+            icon: destinationIcon));
+      });
+    }
+
+    setPolylines() async {
+
+        List<PointLatLng> result = await polylinePoints?.getRouteBetweenCoordinates(
+            googleAPIKey,
+            SOURCE_LOCATION.latitude,
+            SOURCE_LOCATION.longitude,
+            DEST_LOCATION.latitude,
+            DEST_LOCATION.longitude);
+        if (result.isNotEmpty) {
+          // loop through all PointLatLng points and convert them
+          // to a list of LatLng, required by the Polyline
+          result.forEach((PointLatLng point) {
+            polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+          });
+        }
+
+      setState(() {
+          // create a Polyline instance
+          // with an id, an RGB color and the list of LatLng pairs
+          Polyline polyline = Polyline(
+              polylineId: PolylineId("poly"),
+              color: Color.fromARGB(255, 40, 122, 198),
+              points: polylineCoordinates);
+
+          // add the constructed polyline as a set of points
+          // to the polyline set, which will eventually
+          // end up showing up on the map
+          _polylines.add(polyline);
+      });
   }
 }
