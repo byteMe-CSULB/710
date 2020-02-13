d63b1c37f0226ff207bf400ff612b9b271c48066
brandonnhem <brandonnhem@gmail.com>
2020-02-13 00:24:34 -0800

    Revert "Merge master into Directions"
    This reverts commit 74f0e2c2a45102ff4e463984df3ce0aa8fded1bf, reversing
    changes made to 9bc73ecee8a4faf708578b6fec17b09d602c3423.

--- gas_710/lib/main.dart
+++ gas_710/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gas_710/AboutPage.dart';
 import 'package:gas_710/BillingPage.dart';
-import 'package:gas_710/FirestoreTest.dart';
 import 'package:gas_710/InfoPage.dart';
 import 'package:gas_710/NavigationPage.dart';
 import 'package:gas_710/SettingsPage.dart';
@@ -74,19 +73,6 @@ class DrawerCodeOnly extends StatelessWidget {
                   color: Colors.amber),
               ),    
               // Buttons in nav drawer
-
-            ///////////////////// FIREBASE TEST ////////////////////////
-            new ListTile(
-              leading: Icon(Icons.navigation),
-              title: Text("Firestore Test"),
-              onTap: () {
-                Navigator.pop(context); // Navigator.pop(context) will close the navigation drawer
-                Navigator.push(context, new MaterialPageRoute(builder: (context) => new FirestoreTest())); // Navigator.push(context) will send you the page you choose
-              },
-            ),
-
-              ////////////////// FIREBASE TEST ///////////////////////
-
               new ListTile(
                 leading: Icon(Icons.navigation),
                 title: Text("Start trip"),
