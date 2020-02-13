e3a2a7c1706e0297fa4b9bd9d0723ef03040b2d0
Meng Cha <mengcha2194@gmail.com>
2020-02-12 19:06:10 -0800

    Added autocomplete in the search bar.

--- gas_710/lib/NavigationPage.dart
+++ gas_710/lib/NavigationPage.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:gas_710/main.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
+import 'package:geocoder/geocoder.dart';
+import 'package:google_maps_webservice/places.dart';
 
 const double CAMERA_ZOOM = 13;
 const double CAMERA_TILT = 0;
 const double CAMERA_BEARING = 30;
 const LatLng SOURCE_LOCATION = LatLng(33.783022, -118.112858); // CSULB :)
 const LatLng DEST_LOCATION = LatLng(42.6871386, -71.2143403);
+const googlePlacesAPIKey = "AIzaSyD71HMbuRIt7smNaNek_R0OXBRHJMtj_fo";
+
+GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: googlePlacesAPIKey);
 
 class NavigationPage extends StatefulWidget {
   @override
@@ -18,10 +24,11 @@ class NavigationPage extends StatefulWidget {
 
 class _NavigationPageState extends State<NavigationPage> {
   Completer<GoogleMapController> _controller = Completer();
+  TextEditingController _textController = new TextEditingController();
 
   // this set will hold my markers
   Set<Marker> _markers = {};
-  
+
   // this will hold the generated polylines
   Set<Polyline> _polylines = {};
 
@@ -51,8 +58,7 @@ class _NavigationPageState extends State<NavigationPage> {
     sourceIcon = await BitmapDescriptor.fromAssetImage(
         ImageConfiguration(devicePixelRatio: 2.5), 'assets/driving_pin.png');
     destinationIcon = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(devicePixelRatio: 2.5),
-        'assets/location_pin.png');
+        ImageConfiguration(devicePixelRatio: 2.5), 'assets/location_pin.png');
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -64,8 +70,9 @@ class _NavigationPageState extends State<NavigationPage> {
   void _getLocation() async {
     var currentLocation = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
-    print('got current location as ${currentLocation.latitude}, ${currentLocation.longitude}');    
-    var currentAddress = await _getAddress(currentLocation);    
+    print(
+        'got current location as ${currentLocation.latitude}, ${currentLocation.longitude}');
+    var currentAddress = await _getAddress(currentLocation);
     await _moveToPosition(currentLocation);
 
     setState(() {
@@ -80,27 +87,26 @@ class _NavigationPageState extends State<NavigationPage> {
       //   _markers.add(Marker(
       //       markerId: MarkerId('sourcePin'),
       //       position: SOURCE_LOCATION,
-      //       icon: sourceIcon)); 
+      //       icon: sourceIcon));
       final marker = Marker(
         markerId: MarkerId("curr_loc"),
         position: LatLng(currentLocation.latitude, currentLocation.longitude),
-        infoWindow:  InfoWindow(title: currentAddress),
+        infoWindow: InfoWindow(title: currentAddress),
         icon: sourceIcon,
       );
       _markers.add(marker);
-    }); 
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     CameraPosition initialLocation = CameraPosition(
-          zoom: CAMERA_ZOOM,
-          bearing: CAMERA_BEARING,
-          tilt: CAMERA_TILT,
-          target: SOURCE_LOCATION);
+        zoom: CAMERA_ZOOM,
+        bearing: CAMERA_BEARING,
+        tilt: CAMERA_TILT,
+        target: SOURCE_LOCATION);
 
-    return new Scaffold
-    (
+    return new Scaffold(
       drawer: new DrawerCodeOnly(), // provides nav drawer
       appBar: new AppBar(
         title: new Text("Navigation Page"),
@@ -111,50 +117,73 @@ class _NavigationPageState extends State<NavigationPage> {
         backgroundColor: Colors.amber,
         child: Icon(Icons.location_on),
       ),
-      body: Stack(
-        children: <Widget>[
-          GoogleMap(
-            myLocationEnabled: true,
-            myLocationButtonEnabled: false,
-            tiltGesturesEnabled: false,
-            markers: _markers,
-            polylines: _polylines,
-            mapType: MapType.normal,
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: initialLocation,
-          ),
-          Positioned(
-            top: 20.0,
-            right: 15.0,
-            left: 15.0,
-            child: Container(
-              height: 50.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                color: Colors.white,
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter Address..',
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.search), 
-                    iconSize: 30.0,
-                    onPressed: searchandNavigate,
-                  ),
-                ),
-                onChanged: (val) {
-                  setState(() {
-                    searchAddr = val;
-                  });
-                },
-              ),
+      body: Stack(children: <Widget>[
+        GoogleMap(
+          myLocationEnabled: true,
+          myLocationButtonEnabled: false,
+          tiltGesturesEnabled: false,
+          markers: _markers,
+          polylines: _polylines,
+          mapType: MapType.normal,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: initialLocation,
+        ),
+        Positioned(
+          top: 20.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(1.0),
+              color: Colors.white,
             ),
-          )
-        ]
-      ),
+            child: TextField(
+              readOnly: false,
+              controller: _textController,
+              // When user taps search bar, autocomplete comes up
+              onTap: () async {
+                Prediction p = await PlacesAutocomplete.show(
+                    context: context,
+                    mode: Mode.overlay,
+                    apiKey: googlePlacesAPIKey);
+                //if user picks an address, send it to the search bar
+                if (p != null) {
+                  displayPrediction(p);
+                  searchAddr = p.description;
+                  _textController.value = TextEditingValue(
+                    text: searchAddr,
+                    selection: TextSelection.fromPosition(
+                      TextPosition(offset: searchAddr.length),
+                    ),
+                  );
+                  searchandNavigate();
+                }
+              },
+              //When user presses 'ok' on keyboard.
+              onSubmitted: (String value) async {
+                searchandNavigate();
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter Address..',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  iconSize: 30.0,
+                  onPressed: searchandNavigate,
+                ),
+              ),
+              onChanged: (val) {
+                setState(() {
+                  searchAddr = val;
+                });
+              },
+            ),
+          ),
+        )
+      ]),
     );
   }
 
@@ -165,14 +194,23 @@ class _NavigationPageState extends State<NavigationPage> {
   }
 
   searchandNavigate() {
-    Geolocator().placemarkFromAddress(searchAddr).then((result) async { // result is your destination that searched from searchAddr
-      animateTo(result[0].position.latitude, result[0].position.longitude); // takes us to the location
-      var currentLocation = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best); // pings YOUR location
-      double distanceInMeter = await Geolocator().distanceBetween(currentLocation.latitude, currentLocation.longitude, result[0].position.latitude, result[0].position.longitude);
-      print("Distance to $searchAddr is $distanceInMeter meters from your location");
-      setMapPins(currentLocation.latitude, currentLocation.longitude, result[0].position.latitude, result[0].position.longitude);
-      setPolylines(currentLocation.latitude, currentLocation.longitude, result[0].position.latitude, result[0].position.longitude);
+    Geolocator().placemarkFromAddress(searchAddr).then((result) async {
+      // result is your destination that searched from searchAddr
+      animateTo(result[0].position.latitude,
+          result[0].position.longitude); // takes us to the location
+      var currentLocation = await Geolocator().getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best); // pings YOUR location
+      double distanceInMeter = await Geolocator().distanceBetween(
+          currentLocation.latitude,
+          currentLocation.longitude,
+          result[0].position.latitude,
+          result[0].position.longitude);
+      print(
+          "Distance to $searchAddr is $distanceInMeter meters from your location");
+      setMapPins(currentLocation.latitude, currentLocation.longitude,
+          result[0].position.latitude, result[0].position.longitude);
+      setPolylines(currentLocation.latitude, currentLocation.longitude,
+          result[0].position.latitude, result[0].position.longitude);
     });
   }
 
@@ -188,37 +226,36 @@ class _NavigationPageState extends State<NavigationPage> {
   }
 
   Future<void> _moveToPosition(Position pos) async {
-    if(_controller == null) return;
+    if (_controller == null) return;
     print('moving to position ${pos.latitude}, ${pos.longitude}');
     animateTo(pos.latitude, pos.longitude);
   }
 
-  void setMapPins(double sourceLat, double sourceLong, double destLat, double destLong) async {
-      double distanceInMeter = await Geolocator().distanceBetween(sourceLat, sourceLong, destLat, destLong);
-      setState(() {
-        // source pin
-        _markers.add(Marker(
-            markerId: MarkerId('sourcePin'),
-            position: LatLng(sourceLat, sourceLong),
-            icon: sourceIcon));
-        // destination pin
-        _markers.add(Marker(
-            markerId: MarkerId('destPin'),
-            position: LatLng(destLat, destLong),
-            icon: destinationIcon,
-            infoWindow: InfoWindow(
-              title: "$distanceInMeter meters away",
-            )));
-      });
-    }
+  void setMapPins(double sourceLat, double sourceLong, double destLat,
+      double destLong) async {
+    double distanceInMeter = await Geolocator()
+        .distanceBetween(sourceLat, sourceLong, destLat, destLong);
+    setState(() {
+      // source pin
+      _markers.add(Marker(
+          markerId: MarkerId('sourcePin'),
+          position: LatLng(sourceLat, sourceLong),
+          icon: sourceIcon));
+      // destination pin
+      _markers.add(Marker(
+          markerId: MarkerId('destPin'),
+          position: LatLng(destLat, destLong),
+          icon: destinationIcon,
+          infoWindow: InfoWindow(
+            title: "$distanceInMeter meters away",
+          )));
+    });
+  }
 
-  setPolylines(double sourceLat, double sourceLong, double destLat, double destLong) async {
+  setPolylines(double sourceLat, double sourceLong, double destLat,
+      double destLong) async {
     List<PointLatLng> result = await polylinePoints?.getRouteBetweenCoordinates(
-        googleAPIKey,
-        sourceLat,
-        sourceLong,
-        destLat,
-        destLong);
+        googleAPIKey, sourceLat, sourceLong, destLat, destLong);
     if (result.isNotEmpty) {
       // loop through all PointLatLng points and convert them
       // to a list of LatLng, required by the Polyline
@@ -228,17 +265,39 @@ class _NavigationPageState extends State<NavigationPage> {
     }
 
     setState(() {
-        // create a Polyline instance
-        // with an id, an RGB color and the list of LatLng pairs
-        Polyline polyline = Polyline(
-            polylineId: PolylineId("poly"),
-            color: Color.fromARGB(255, 40, 122, 198),
-            points: polylineCoordinates);
+      // create a Polyline instance
+      // with an id, an RGB color and the list of LatLng pairs
+      Polyline polyline = Polyline(
+          polylineId: PolylineId("poly"),
+          color: Color.fromARGB(255, 40, 122, 198),
+          points: polylineCoordinates);
 
-        // add the constructed polyline as a set of points
-        // to the polyline set, which will eventually
-        // end up showing up on the map
-        _polylines.add(polyline);
+      // add the constructed polyline as a set of points
+      // to the polyline set, which will eventually
+      // end up showing up on the map
+      _polylines.add(polyline);
     });
+  }
+
+  //prints the autocomplete address
+  //Can be scraped later if of no use
+  Future<Null> displayPrediction(Prediction p) async {
+    if (p != null) {
+      PlacesDetailsResponse detail =
+          await _places.getDetailsByPlaceId(p.placeId);
+
+      var placeId = p.placeId;
+      double lat = detail.result.geometry.location.lat;
+      double lng = detail.result.geometry.location.lng;
+
+      var address = await Geocoder.local.findAddressesFromQuery(p.description);
+
+      print(lat.toString() + ", " + lng.toString() + ", " + p.description);
+    }
+  }
+
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
   }
 }
