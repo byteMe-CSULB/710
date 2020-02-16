fa05dc9b066e89c15445af6346465d24b00e3eb0
taylor-meyer <taylor.ross.meyer@gmail.com>
2020-02-15 20:15:51 -0800

    Add button that opens a webpage

--- gas_710/lib/LinkPaymentPage.dart
+++ gas_710/lib/LinkPaymentPage.dart
@@ -1,23 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:gas_710/main.dart';
+import 'package:device_apps/device_apps.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:android_intent/android_intent.dart';
 
 class LinkPaymentPage extends StatelessWidget {
+
+  _launchURL() async {
+    const url = 'https://flutter.io';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold
-    (
-      drawer: new DrawerCodeOnly(), // provides the nav drawer
-      appBar: new AppBar(
-        title: new Text("Contacts Page"),
-        backgroundColor: Colors.purple,
+    return new Scaffold(
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: _launchURL,
+          child: new Text('Show Flutterr homepage'),
+        ),
       ),
-       body: new Container(
-        child: new Center(
-          child: new Text("This is the link to payment page",
-          style: TextStyle(fontStyle: FontStyle.italic, fontSize: 25),
-          )
-        )
-      )
     );
   }
 }
+
+
+/*
+
+_openJioSavaan (data) async
+{String dt = data['JioSavaan'] as String;
+  bool isInstalled = await DeviceApps.isAppInstalled('com.jio.media.jiobeats');
+if (isInstalled != false)
+ {
+    AndroidIntent intent = AndroidIntent(
+      action: 'action_view',
+      data: dt
+  );
+  await intent.launch();
+ }
+else
+  {
+  String url = dt;
+  if (await canLaunch(url))
+    await launch(url);
+   else
+    throw 'Could not launch $url';
+}
+}
+
+ */
