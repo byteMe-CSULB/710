3f51286a9450181148334546750345aa05064464
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-02-23 01:15:47 -0800

    Write to JSON file

--- scrape.py
+++ scrape.py
@@ -5,8 +5,8 @@
 # Libraries
 import requests
 from urllib.request import Request, urlopen
-import time
 from bs4 import BeautifulSoup as soup
+import json
 
 # AAA website to be scraped
 url = "https://gasprices.aaa.com/?state=CA"
@@ -18,11 +18,33 @@ req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
 
 # Parse it
-page_soup = soup(webpage, "html.parser")
+page_soup = soup(webpage, 'html.parser')
 
 # Put every line of html that contains an <h3> tag
 # into a list
-head3 = page_soup.findAll("h3")
+h3 = page_soup.findAll('h3')
 
 # Element 5 is "Los Angeles-Long Beach"
-print(head3[5])
+#print(head3)
+
+# For JSON
+data = {}
+data['fuelCost'] = []
+
+# For every tag
+for i in h3:
+    # Get location
+    loc = i.text
+    # Get fuel cost
+    cost = i.attrs.get('data-cost')
+    #print(loc, cost, sep=' ')
+
+    # Append to the JSON
+    data['fuelCost'].append({
+        'location': loc,
+        'cost': cost
+    })
+
+# Write JSON to .txt file
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
