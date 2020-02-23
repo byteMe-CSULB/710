f63ed00649b3ca59493beea0f38cfce86b55f920
taylor-meyer <taylor.ross.meyer@gmail.com>
2020-02-22 18:28:25 -0800

    Add WebScraper Python file

--- scrape.py
+++ scrape.py
@@ -4,8 +4,18 @@ Let's learn how to web scrape!
 
 # Libraries
 import requests
-import urllib.request
+from urllib.request import Request, urlopen
 import time
 from bs4 import BeautifulSoup
 
 print("Hello World!")
+
+url = "https://gasprices.aaa.com/?state=CA"
+
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+webpage = urlopen(req).read()
+
+#response = requests.get(url)
+#soup = BeautifulSoup(response.text, "html.parser")
+print(webpage)
