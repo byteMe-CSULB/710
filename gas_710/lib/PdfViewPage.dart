39dedc028dacd4712e9a3b4f83464a9a6fecac10
brandonnhem <brandonnhem@gmail.com>
2020-03-16 04:30:38 -0700

    (Android only) share PDF file with any app/anyone

--- gas_710/lib/PdfViewPage.dart
+++ gas_710/lib/PdfViewPage.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
+import 'package:flutter_share/flutter_share.dart';
 
 class PdfViewPage extends StatelessWidget {
   final String path;
   const PdfViewPage({Key key, this.path}) : super(key: key);
+
+  Future<void> shareFile() async {
+    await FlutterShare.shareFile(
+      title: 'Reciept PDF',
+      text: 'Here is a receipt of the most recent trips we took together',
+      filePath: path,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +20,18 @@ class PdfViewPage extends StatelessWidget {
       path: path,
       appBar: AppBar(
         title: Text(
-          'PDF File'
+          'PDF Document'
         ),
         backgroundColor: Colors.purple,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              print('Sharing file'); 
+              shareFile(); // this does NOT work on iPhone
+            }
+          )
+        ],
       )
     );
   }
