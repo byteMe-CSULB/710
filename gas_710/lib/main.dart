7de1583c165acb5ffb9cbe76649dd184c6a71b28
brandonnhem <brandonnhem@gmail.com>
2020-03-25 21:37:38 -0700

    Splash screen and app intro added

--- gas_710/lib/main.dart
+++ gas_710/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gas_710/AboutPage.dart';
 import 'package:gas_710/BillingPage.dart';
 import 'package:gas_710/InfoPage.dart';
@@ -7,6 +8,9 @@ import 'package:gas_710/SettingsPage.dart';
 import 'package:gas_710/ContactsPage.dart';
 import 'package:gas_710/WebViewPage.dart';
 import 'package:gas_710/auth.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -14,30 +18,214 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: NavigationPage(),
+      home: SplashScreen(),
     );
   }
 }
 
-class GasApp extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() => new SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+    if (_seen) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => NavigationPage()));
+    } else {
+      await prefs.setBool('seen', true);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => OnBoardingPage()));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    new Timer(new Duration(milliseconds: 300), () {
+    checkFirstSeen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        drawer: DrawerCodeOnly(),
-        appBar: AppBar(
-          title: Text('7 ! 0'),
-          backgroundColor: Colors.purple,
-        ),
-        body: new Container(
-            child: new Center(
-                child: new Text(
-          "This is the main page",
-          style: TextStyle(fontStyle: FontStyle.italic, fontSize: 25),
-        ))));
+      return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo( // eventually we should replace this with our actual logo
+              size: 100
+            ),
+            Text('710 by byteMe')
+          ]
+        ), 
+      ),
+    );
   }
 }
 
-class DrawerCodeOnly extends StatelessWidget {
+class OnBoardingPage extends StatefulWidget {
+  @override
+  _OnBoardingPageState createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => NavigationPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      pages: [
+        PageViewModel(
+          title: "Rideshare with friends",
+          body: 
+            "Instead of picking up random strangers, pick up your friends or family!",
+          image: Align(
+            child: Image.asset('assets/car.png', width: 350.0),
+            alignment: Alignment.bottomCenter
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Search for destinations",
+          body:
+            "Look any place that you would like to go with your passengers.",
+          image: Align(
+            child: Image.asset('assets/tour.png', width: 350.0),
+            alignment: Alignment.bottomCenter
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Keep all records",
+          body:
+            "We save everything for you to make it easier to track.",
+          image: Align(
+            child: Image.asset('assets/folder.png', width: 350.0),
+            alignment: Alignment.bottomCenter
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Send a bill",
+          body: "If someone owes you anything, send them a little nudge with our receipts.",
+          image: Align(
+            child: Image.asset('assets/invoice.png', width: 350.0),
+            alignment: Alignment.bottomCenter
+          ),
+          footer: _signInButton(),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () {
+        if(signedIn) {
+          _onIntroEnd(context);
+        } else {
+          Fluttertoast.showToast(
+            msg: 'Please sign in before continuing',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIos: 1,
+            fontSize: 16.0,
+          );
+        }
+      },
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeColor: Colors.purple,
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+    );
+  }
+
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        signInWithGoogle().whenComplete(() {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return NavigationPage();
+              },
+            ),
+          );
+        });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 4,
+      highlightedBorderColor: Colors.purple[400],
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/google_logo.png"), height: 35.0), // asset image
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerCodeOnly extends StatefulWidget {
+  @override
+  DrawerCodeOnlyState createState() => DrawerCodeOnlyState();
+}
+
+class DrawerCodeOnlyState extends State<DrawerCodeOnly> {
+  bool _signedIn = false;
+  Future checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      return _signedIn = prefs.getBool('signedIn');
+    });
+  }
+
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Drawer(
