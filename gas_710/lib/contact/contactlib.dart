f790f04fca42e49e8022e9f55f358c84575c51a4
brandonnhem <brandonnhem@gmail.com>
2020-05-16 15:31:15 -0700

    Removed unused files

--- gas_710/lib/contact/contactlib.dart
+++
@@ -1,2 +0,0 @@
-export 'contact.dart';
-export 'contact_db.dart';
--- gas_710/lib/contact/global_contact.dart
+++
@@ -1,9 +0,0 @@
-
-const String dbName = 'contact.db';
-const String table = 'Contact';
-const String colID = 'id';
-const String colName = 'name';
-const String colPhone = 'phone';
-const String colEmail = 'email';
-const String colFraction = 'fraction';
-const String colSelect = 'onTrip';
